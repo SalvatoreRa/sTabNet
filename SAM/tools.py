95a7ec9892592dc33c0179ff4a4d43e3536f56f0
Salvatore Raieli <32837577+SalvatoreRa@users.noreply.github.com>
2023-06-06 10:39:50 +0200

    Update tools.py

--- SAM/tools.py
+++ SAM/tools.py
@@ -228,7 +228,14 @@ def splitting_data( data_x = None, data_y = None, val_split = 0.1, test_split = 
 def classification_metrics(_X_test = None, _model = None, _y_test = None, nn= True):
     '''
     simple wrapper for getting all the classification metrics
-    
+    used for binary classsification
+    Parameters:
+    - test set, panda dataframe or numpy array
+    - model, a trained model
+    - y_test, the target variable,  panda series or numpy array or a list
+    -nn, bool, if it is a neural network or scikit-learn like algorithm
+    return
+    - a list of classification metrics result
     ---
     example of usage:
     classification_metrics(_X_test = X_test, _model = model_go, _y_test = y_test_enc, nn= True)
